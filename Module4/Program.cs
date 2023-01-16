cfdab115bb26db711418b4dbc6cc4d74cb816bd4
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-16 20:48:05 +0300

    Задание 4.1.18

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -10,46 +10,51 @@ namespace Module4
 
 			var color = Console.ReadLine().ToLower();
 
-			if (color == "красный")
-			{
-				Console.BackgroundColor = ConsoleColor.Red;
-				Console.ForegroundColor = ConsoleColor.Black;
-
-				Console.WriteLine("Твой цвет - Красный!");
-			}
-
-			else if (color == "зеленый")
-			{
-				Console.BackgroundColor = ConsoleColor.Green;
-				Console.ForegroundColor = ConsoleColor.Black;
-
-				Console.WriteLine("Твой цвет - Зеленый!");
-			}
-			
-			else if (color == "розовый")
-			{
-				Console.BackgroundColor = ConsoleColor.Magenta;
-				Console.ForegroundColor = ConsoleColor.Black;
-
-				Console.WriteLine("Твой цвет - Розовый!");
-			}
-
-			else if (color == "серый")
-			{
-				Console.BackgroundColor = ConsoleColor.DarkGray;
-				Console.ForegroundColor = ConsoleColor.Black;
-
-				Console.WriteLine("Твой цвет - Серый!");
-			}
-
-			else
+            switch (color)
             {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
+				case "красный":
+					Console.BackgroundColor = ConsoleColor.Red;
+					Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Твой цвет - Бирюзовый!");
-            }
+					Console.WriteLine("Твой цвет - Красный!");
+					break;
 
+				case "зеленый":
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Твой цвет - Зеленый!");
+					break;
+
+				case "розовый":
+					Console.BackgroundColor = ConsoleColor.Magenta;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Твой цвет - Розовый!");
+					break;
+
+				case "серый":
+					Console.BackgroundColor = ConsoleColor.DarkGray;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Твой цвет - Серый!");
+					break;
+
+				case "бирюзовый":
+					Console.BackgroundColor = ConsoleColor.Cyan;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Твой цвет - Бирюзовый!");
+					break;
+
+				default:
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.Red;
+
+					Console.WriteLine("Твой цвет - Другой!");
+					break;
+			}
+	
 			Console.ReadKey();
         }
     }
