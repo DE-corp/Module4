7aac38549efd4d5f384ac09edd20afe93b58b7bf
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-16 20:24:19 +0300

    Задание 4.1.14

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -6,17 +6,23 @@ namespace Module4
     {
         static void Main(string[] args)
         {
-            var a = 6;
-            var b = 7;
+			var a = 6;
+			var b = 7;
 
-            if (a == b)
-            {
-                Console.WriteLine("Условие истинно");
-            }
-            else
-            {
-                Console.WriteLine("Условие ложно");
-            }
+			if (a == b && b > 1)
+			{
+				Console.WriteLine("Условие истинно");
+			}
+
+			else if (b > 10 || b == 7)
+			{
+				Console.WriteLine("Значение b = {0} больше 10 или равно 7", b);
+			}
+			else
+			{
+				Console.WriteLine("Значение b = {0}", b);
+			
+			}
                 Console.ReadKey();
         }
     }
